5cfaa0be0837a54987438ef940b1625a8027625d
“soniatan” <sonnyeah@gmail.com>
2021-10-12 22:26:34 +0800

    added search button on closest MRT selection page to fetch from API and only display results matching closest MRT station. attempting to add more logic to keep calling data until there are at least 3 diff restaurants that match closest MRT selection

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import './App.css';
-import React from "react"
-import Navbar from "./components/Navbar"
-import {Route, Link, NavLink} from "react-router-dom"
-import CssBaseline from "@mui/material/CssBaseline"
-import {Stack} from "@mui/material"
-import Keyword from "./components/Keyword"
-import Button from "@mui/material/Button"
-import ClosestMRT from './components/ClosestMRT';
+import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Link } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Stack } from "@mui/material";
+import Keyword from "./components/Keyword";
+import Button from "@mui/material/Button";
+import ClosestMRT from "./components/ClosestMRT";
 
 function App() {
-
-
   return (
     <>
-    <CssBaseline />
-    <div className="App">
-      <Navbar />
-      <Route exact path="/">
-      <h1>Welcome to SG Food Finder</h1>
-      <h3>Search up restaurants and other food establishments by one of the following: </h3>
-      <Stack direction="row" spacing={2} justifyContent="center">
-      <Button variant="contained">
-        <Link to="/keyword">Search by Keyword</Link>
-      </Button>
-      <Button variant="contained">
-        <Link to="/closestMRT">Search by nearest MRT</Link>
-      </Button>
-      </Stack>
-      </Route>
-      <Route path="/keyword">
-        <Keyword />
-      </Route>
-      <Route path="/closestMRT">
-        <ClosestMRT />
-      </Route>
+      <CssBaseline />
+      <div className="App">
+        <Navbar />
+        <Route exact path="/">
+          <h1>Welcome to SG Food Finder</h1>
+          <h3>
+            Search up restaurants and other food establishments by one of the
+            following:{" "}
+          </h3>
+          <Stack direction="row" spacing={2} justifyContent="center">
+            <Button variant="contained">
+              <Link to="/keyword">Search by Keyword</Link>
+            </Button>
+            <Button variant="contained">
+              <Link to="/closestMRT">Search by nearest MRT</Link>
+            </Button>
+          </Stack>
+        </Route>
+        <Route path="/keyword">
+          <Keyword />
+        </Route>
+        <Route path="/closestMRT">
+          <ClosestMRT />
+        </Route>
       </div>
     </>
   );
--- src/components/ClosestMRT.jsx
+++ src/components/ClosestMRT.jsx
@@ -1,86 +1,180 @@
-import React, {useState} from "react"
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import mrtStations from "../data/mrtStations"
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Button from "@mui/material/Button";
+import mrtStations from "../data/mrtStations";
+// import SearchMRT from "./SearchMRT"
 
-const ClosestMRT = () => {
-
+const ClosestMRT = (props) => {
   const [line, setLine] = useState("");
   const [station, setStation] = useState("");
+  const [input, setInput] = useState([]);
 
-      const selectedLineStations = mrtStations.data.filter(selected => selected.lineName === line)
-      const stations = selectedLineStations.map((stations) => {
-        return (
-        <MenuItem key={stations.stationName} value={stations.stationName}>{stations.stationName}</MenuItem>
-      )})
+  const [url, setUrl] = useState(
+    "https://tih-api.stb.gov.sg/content/v1/food-beverages/search?keyword=" +
+      "food" +
+      "&language=en&apikey=" +
+      process.env.REACT_APP_TIH_API_KEY
+  );
 
-    const handleLineSelect = (event) => {
-      setLine(event.target.value)
+  const selectedLineStations = mrtStations.data.filter(
+    (selected) => selected.lineName === line
+  );
+  const stations = selectedLineStations.map((stations) => {
+    return (
+      <MenuItem key={stations.stationName} value={stations.stationName}>
+        {stations.stationName}
+      </MenuItem>
+    );
+  });
+
+  const handleLineSelect = (event) => {
+    setLine(event.target.value);
+  };
+
+  const handleStationSelect = (event) => {
+    setStation(event.target.value);
+  };
+
+  const handleSubmit = async () => {
+    console.log("handling submit")
+    const res = await fetch(url);
+    let data = await res.json();
+    let token = data?.nextToken
+    console.log("unfiltered", data);
+    const restoData = data?.data.filter(
+      (search) =>
+        search.nearestMrtStation === station ||
+        search.nearestMrtStation === station + " Station" ||
+        search.nearestMrtStation === station + " MRT Station"
+    );
+    setInput(restoData)
+    console.log("filtered", restoData)
+    console.log("new input", input)
+    for (let i=0; i<5; i++) {
+    if (input.length < 3) {
+      console.log("iterating")
+      console.log("first token", token)
+      let newRes = await fetch(
+        "https://tih-api.stb.gov.sg/content/v1/food-beverages/search?keyword=food&nextToken=" +
+          token +
+          "%3D&language=en&apikey=" +
+          process.env.REACT_APP_TIH_API_KEY
+      );
+      let newData = await newRes.json();
+      console.log("next page", newData)
+      token=newData.nextToken
+      let newRestoData = newData?.data.filter(
+        (search) =>
+          search.nearestMrtStation === station ||
+          search.nearestMrtStation === station + " Station" ||
+          search.nearestMrtStation === station + " MRT Station"
+      );
+      console.log("new token", token)
+      console.log("newRestoData", newRestoData)
+      console.log("second call input", input)
+      return (
+        setInput(newRestoData, ...restoData)
+      ) 
+    } 
     }
+  };
 
-    const handleStationSelect = (event) => {
-      setStation(event.target.value)
-    }
+  const lineSelectStyle = {
+    backgroundColor:
+      line === "East West Line"
+        ? "green"
+        : line === "North South Line"
+        ? "red"
+        : line === "North East Line"
+        ? "purple"
+        : line === "Circle Line"
+        ? "#ffcc33"
+        : line === "Downtown Line"
+        ? "blue"
+        : "none",
+    color: "white",
+  };
 
-    const lineSelectStyle = {
-      backgroundColor: line === "East West Line" 
-      ? "green" 
-      : line === "North South Line" 
-      ? "red"
-      : line === "North East Line"
-      ? "purple"
-      : line === "Circle Line" 
-      ? "#ffcc33" 
-      : line === "Downtown Line" 
-      ? "blue"
-      : "lightgrey", 
-      color: "white"
-      }
+  return (
+    <>
+      <p>Search by closest MRT selection</p>
 
-    return(
-      <>
-        <p>Closest MRT selection</p>
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl sx={{ width: 300 }}>
+          <InputLabel>Select MRT Line</InputLabel>
+          <Select
+            id="mrtLineSelect"
+            value={line}
+            label="MRT Line"
+            onChange={handleLineSelect}
+            variant={"standard"}
+            sx={lineSelectStyle}
+          >
+            <MenuItem
+              value={"East West Line"}
+              sx={{ backgroundColor: "rgba(0,116,16,1)", color: "white" }}
+            >
+              East West Line
+            </MenuItem>
+            <MenuItem
+              value={"North South Line"}
+              sx={{ backgroundColor: "red", color: "white" }}
+            >
+              North South Line
+            </MenuItem>
+            <MenuItem
+              value={"North East Line"}
+              sx={{ backgroundColor: "purple", color: "white" }}
+            >
+              North East Line
+            </MenuItem>
+            <MenuItem
+              value={"Circle Line"}
+              sx={{ backgroundColor: "#ffcc33", color: "white" }}
+            >
+              Circle Line
+            </MenuItem>
+            <MenuItem
+              value={"Downtown Line"}
+              sx={{ backgroundColor: "blue", color: "white" }}
+            >
+              Downtown Line
+            </MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
 
-        <Box sx={{ minWidth: 120 }}>
-      <FormControl sx={{width: 300}}>
-        <InputLabel>Select MRT Line</InputLabel>
-        <Select
-          id="mrtLineSelect"
-          value={line}
-          label="MRT Line"
-          onChange={handleLineSelect}
-          variant={"standard"}
-          sx={lineSelectStyle}
-        >
-          <MenuItem value={"East West Line"} sx={{backgroundColor: "rgba(0,116,16,1)", color: "white"}}>East West Line</MenuItem>
-          <MenuItem value={"North South Line"} sx={{backgroundColor: "red", color: "white"}}>North South Line</MenuItem>
-          <MenuItem value={"North East Line"} sx={{backgroundColor: "purple", color: "white"}}>North East Line</MenuItem>
-          <MenuItem value={"Circle Line"} sx={{backgroundColor: "#ffcc33", color: "white"}}>Circle Line</MenuItem>
-          <MenuItem value={"Downtown Line"} sx={{backgroundColor: "blue", color: "white"}}>Downtown Line</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+      <Box sx={{ minWidth: 120, margin: "20px 0 0 0" }}>
+        <FormControl sx={{ width: 300 }}>
+          <InputLabel>Select Station</InputLabel>
+          <Select
+            id="stationSelect"
+            value={station}
+            label="Select Station"
+            onChange={handleStationSelect}
+            variant={"standard"}
+          >
+            {stations}
+          </Select>
 
-    <Box sx={{ minWidth: 120, marginTop:"20px" }}>
-      <FormControl sx={{width: 300}}>
-        <InputLabel>Select Station</InputLabel>
-        <Select
-          id="stationSelect"
-          value={station}
-          label="Select Station"
-          onChange={handleStationSelect}
-          variant={"standard"}
-        >
-          {stations}
-        </Select>
-      </FormControl>
-    </Box>
+          <Button
+            variant="contained"
+            onClick={handleSubmit}
+            disableElevation
+            sx={{ margin: "15px 0 0 0", backgroundColor: "lightgrey" }}
+          >
+            Search
+          </Button>
+        </FormControl>
+      </Box>
+      <br />
+      <h2>{input?.[0]?.name}</h2>
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default ClosestMRT
+export default ClosestMRT;
